6f7f2a998bab2340fc35dae6e8013089fed9d485
Gregory <gregoryhunkeler@gmail.com>
2022-09-19 12:14:55 -0300

    b

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ const cors = require("cors");
 const express = require("express");
 const routes = require("./routes");
 const APP_PORT = process.env.APP_PORT || 3000;
-const dbInitialSetup = require("./dbInitialSetup");
+// const dbInitialSetup = require("./dbInitialSetup");
 const app = express();
 
-app.use(express.json())
-app.use(express.static(__dirname + "/public"));
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
+routes(app);
 
 // dbInitialSetup();
-routes(app);
 
 app.listen(APP_PORT, () => {
   console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}.`);
