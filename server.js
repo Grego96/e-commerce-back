95346a574fe98078ca6673e125f2bc0ca2791a29
Manuel Perera <manuel.perera@student.ha.dev>
2022-09-20 13:54:15 -0300

    comment seeders

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 routes(app);
 
-dbInitialSetup();
+// dbInitialSetup();
 
 app.listen(APP_PORT, () => {
   console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}.`);
