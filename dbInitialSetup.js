b24fbd0487ca656d7ce86cb00ce799a75a0086f1
Manuel Perera <manuel.perera@student.ha.dev>
2022-09-22 16:51:44 -0300

    test

--- dbInitialSetup.js
+++ dbInitialSetup.js
@@ -4,10 +4,10 @@ module.exports = async () => {
   // Crear tablas:
   await db.sequelize.sync({ force: true });
   console.log("[Database] ¡Las tablas fueron creadas!");
-  await require("./seeders/categorySeeders")();
-  await require("./seeders/productSeeders")();
-  await require("./seeders/userSeeders")();
   await require("./seeders/orderSeeders")();
+  await require("./seeders/userSeeders")();
+  await require("./seeders/productSeeders")();
+  await require("./seeders/categorySeeders")();
 
   console.log("[Database] ¡Los datos de prueba fueron insertados!");
 };
